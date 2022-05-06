ee2b877adf4c21a4b1398c644ade677c2aa92eae
saladito <cesarborjamoreno@gmail.com>
2022-05-06 18:04:05 +0200

    a

--- src/ServerA.java
+++ src/ServerA.java
@@ -1,8 +1,49 @@
-import java.rmi.Remote;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
-public interface ServerA extends Remote
-{
-    public String dar_hora() throws RemoteException;
-    public String dar_fecha() throws RemoteException;
+public class ServerA {
+
+    private static String ipHost;
+    private static final String ipBroker = "localhost";
+
+    public ServerA(String ipHost) throws RemoteException {
+        super();
+        this.ipHost = ipHost;
+    }
+
+    public String dar_hora() {
+        DateFormat dateF = new SimpleDateFormat("HH:mm:ss");
+		Date date = new Date();
+		return dateF.format(date);
+    }
+
+    public String dar_fecha() {
+        DateFormat dateF = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		return dateF.format(date);
+    }
+
+    public static void main(String args[]) {
+        System.setProperty("java.security.policy", "./src/java.policy");
+        System.setSecurityManager(new SecurityManager());
+        String hostName = "serverA";
+        String brokerName = "";
+
+        Broker broker = (Broker) Naming.lookup("//" + brokerName + "");
+        try {
+            ServerA o = new ServerA(args[0]);
+            System.out.println("Creado!");
+
+            //Registrar el servidor en el broker
+            broker.registrar_servidor(hostName,brokerName);
+
+            System.out.println("Estoy registrado en el Broker!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
