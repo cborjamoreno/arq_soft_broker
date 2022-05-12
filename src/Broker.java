968911ee2df9aa7a80e66bb155e3b39f7d9d60e7
saladito <cesarborjamoreno@gmail.com>
2022-05-12 21:37:17 +0200

    cosas de comentarios

--- src/Broker.java
+++ src/Broker.java
@@ -11,7 +11,7 @@ public interface Broker extends Remote {
     * @param parametros         Parametros del servicio
     * @return                   Valor devuelto serializado como String
     */
-    String ejecutar_servicio(String metodo, Object[] parametros) throws RemoteException;
+    public String ejecutar_servicio(String nombre_servicio, Object[] parametros) throws RemoteException;
 
     /**
     * Registra un servidor en el broker
@@ -20,25 +20,25 @@ public interface Broker extends Remote {
     * @param host_remoto_IP_puerto  IP del servidor 
     * @return                       void
     */
-    void registrar_servidor(String host_remoto_IP_port, String nombre_registrado) throws RemoteException;
+    public void registrar_servidor(String nombre_servidor, String host_remoto_IP_puerto) throws RemoteException;
 
     /**
     * Registra un servicio en el broker
     *
     * @param nombre_registrado  Nombre del servidor que ofrece este servicio
-    * @param nom_servicio       Nombre del servicio a registrar
+    * @param nombre_servicio       Nombre del servicio a registrar
     * @param tipo_retorno       Tipo que devuelve el servicio
     * @param tipoParametros     Tipo de los parametros del servicio
     * @return                   void
     */
-    public void registrar_servicio(String nombre_regitrado, String nom_servicio, String tipo_retorno, String[] tipoParametros) throws RemoteException;
+    public void registrar_servicio(String nombre_registrado, String nombre_servicio, String tipo_retorno, String[] tipo_parametros) throws RemoteException;
     
     /**
     * Lista los servicios disponibles en el broker
     * 
     * @return       ArrayList de servicios
     */
-    ArrayList<Servicio> listar_servicios() throws RemoteException;
+    public ArrayList<Servicio> listar_servicios() throws RemoteException;
 
     /**
     * Da de baja un servicio en el broker
